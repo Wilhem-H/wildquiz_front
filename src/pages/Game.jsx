6733f689df09f458171f9d4de1bdab8510c06153
Wilhem-H <wilhem.hafsa@outlook.fr>
2023-09-25 10:34:20 +0200

    first css version

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,6 +1,22 @@
+import { questions } from "../utils/quizData.js";
 import "./Game.css";
 
 export function Game() {
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  // Mélanger les questions
+  shuffleArray(questions);
+
+  // Sélectionner les 10 premières questions
+  const currentQuestions = questions.slice(0, 10);
+
+  console.log(currentQuestions);
+
   return (
     <div className="game">
       <div className="question"> Question ici</div>
